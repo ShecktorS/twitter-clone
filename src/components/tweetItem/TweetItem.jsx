636e69645cb1127f41ddcb869542de30b8849244
ShecktorS <ettoresanfilippo@gmail.com>
2023-03-06 18:56:59 +0100

    Tweet structure completion

--- src/components/tweetItem/TweetItem.jsx
+++ src/components/tweetItem/TweetItem.jsx
@@ -13,14 +13,28 @@ const TweetItem = () => {
         </div>
         <div className="tweet__text">{tweets[1].body}</div>
         <div className="tweet__icons">
-          <img src="" alt=""></img>
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
+          <img
+            src="https://img.icons8.com/external-tanah-basah-basic-outline-tanah-basah/256/external-comments-social-media-ui-tanah-basah-basic-outline-tanah-basah.png"
+            alt="comment image"
+          ></img>
+          <img
+            src="https://img.icons8.com/external-bearicons-glyph-bearicons/256/external-Repost-social-media-bearicons-glyph-bearicons.png"
+            alt="repost image"
+          />
+          <img
+            src="https://img.icons8.com/material-outlined/256/filled-like.png"
+            alt="like image"
+          />
+          <img
+            src="https://img.icons8.com/ios-glyphs/256/bar-chart.png"
+            alt="analytics image"
+          />
+          <img
+            src="https://img.icons8.com/material-outlined/256/share.png"
+            alt="shared image"
+          />
         </div>
       </div>
-      <hr />
     </div>
   );
 };
