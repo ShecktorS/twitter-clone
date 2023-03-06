ba027437ddb9ff5a32f7434c29c80483f21d5df4
ShecktorS <ettoresanfilippo@gmail.com>
2023-03-06 15:22:58 +0100

    Organisation of the application structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>pippo</h1>
+    </div>
+  );
 }
 
 export default App;
